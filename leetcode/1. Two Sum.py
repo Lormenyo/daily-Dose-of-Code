4649a3442a57549d27c17887a212bd4f0c867ada
Hannah Lormenyo <lormenyoh@gmail.com>
2022-09-06 20:25:06 +0100

    completed two sum

---
+++ leetcode/1. Two Sum.py
@@ -0,0 +1,64 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+
+class Solution(object):
+    def twoSum1(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        for i, num in enumerate(nums):
+            numDifference = target - num
+            nums[i] = 'x'
+            searchIndex = nums.index(numDifference) if numDifference in nums else -1
+            if searchIndex != -1:
+                return [i, searchIndex]
+
+        # Runtime: 1864 ms, faster than 41.68% of Python online submissions for Two Sum.
+        # Memory Usage: 14.1 MB, less than 85.90% of Python online submissions for Two Sum.
+
+
+
+    def twoSum2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        for i, num in enumerate(nums):
+            if target - num in nums and i != nums.index(target-num):
+                return [i, nums.index(target-num)]
+
+        # Runtime: 784 ms
+        # Memory Usage: 13.5 MB
+
+
+
+sol = Solution()
+print(sol.twoSum([2,7,11,15], 9))
+print(sol.twoSum([3,2,4], 6))
+print(sol.twoSum([3,3], 6))
+
