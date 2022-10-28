482f4e562c48d94b36119487f19463ed69774e47
Hannah Lormenyo <lormenyoh@gmail.com>
2022-10-29 00:01:12 +0100

    finally solved 2d array problem

--- HackerRank/Arrays/2DArray.py
+++ HackerRank/Arrays/2DArray.py
@@ -6,9 +6,9 @@ def hourglassSum(arr):
         for column in range(4):
             # print(row, column)
             subArray = [a[column:column+3] for a in arr[row:row+3]]
-            # print(subArray)
-            sums.append(sum(sum(subArray, [])))
-    
+            # print(f"sub: {subArray}")
+            sums.append(sum(sum(subArray, [])) - subArray[1][0] - subArray[1][2])
+    # print(sums)
     return max(sums)
 
 
@@ -20,6 +20,24 @@ arr = [
     [0, 0, 0, 2, 0, 0],
     [0, 0, 1, 2, 4, 0]
 ]
-print()
+arr2 = [
+    [1, 1, 1, 0 ,0, 0],
+    [0, 1, 0, 0 ,0, 0],
+    [1, 1, 1, 0 ,0, 0],
+    [0, 9, 2, -4, -4, 0],
+    [0, 0, 0, -2, 0, 0],
+    [0, 0, -1, -2, -4, 0]
+]
+
+arr3 = [
+    [-9, -9, -9,  1, 1, 1], 
+    [0, -9,  0,  4, 3, 2],
+    [-9, -9, -9,  1, 2, 3],
+    [0,  0,  8,  6, 6, 0],
+    [0,  0,  0, -2, 0, 0],
+    [0 , 0 , 1 , 2 ,4 ,0],
+]
 
 print(f"max: {hourglassSum(arr)}")
+print(f"max: {hourglassSum(arr2)}")
+print(f"max: {hourglassSum(arr3)}")
