6edad56644049bc89870b5e7921247954e80ced7
Hannah Lormenyo <lormenyoh@gmail.com>
2022-10-30 19:35:50 +0000

    added the question

--- HackerRank/Arrays/rotLeft.py
+++ HackerRank/Arrays/rotLeft.py
@@ -1,9 +1,9 @@
-# A left rotation operation on an array shifts each of the array's elements  
-# unit to the left. For example, if  left rotations are performed on array , 
-# then the array would become . Note that the lowest index item moves to the 
+# A left rotation operation on an array shifts each of the array's elements  1
+# unit to the left. For example, if  left rotations are performed on array  [1,2,3,4,5], 
+# then the array would become [3,4,5,1,2]. Note that the lowest index item moves to the 
 # highest index in a rotation. This is called a circular array.
 
-# Given an array  of  integers and a number, , perform  left rotations on the 
+# Given an array a of n integers and a number, d, perform  d left rotations on the 
 # array. Return the updated array to be printed as a single line of space-separated 
 # integers.
 
