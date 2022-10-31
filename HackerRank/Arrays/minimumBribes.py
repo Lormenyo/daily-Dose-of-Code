f37d6645b8be68494074a1550022e836f2d08918
Hannah Lormenyo <lormenyoh@gmail.com>
2022-10-31 21:30:47 +0000

    solved question but questioning its validity

--- HackerRank/Arrays/minimumBribes.py
+++ HackerRank/Arrays/minimumBribes.py
@@ -2,22 +2,26 @@
 
 def minimumBribes(q):
     numberOfBribes = 0
+    isTooChaotic = False
     
     for index, number in enumerate(q):
-        # print(index, number)
         if index < number - 1:
-            numberOfBribes += (number - 1 - index)
-            # print(f"number of bribes: {numberOfBribes}")
-        if index > number -1:
-            numberOfBribes += (index - number)
-            # print(f"number of bribes: {numberOfBribes}")
-      
-
+            bribesTaken = (number - 1 - index) 
             
-    if numberOfBribes > 2:
-        print(numberOfBribes)
+            if bribesTaken > 2:
+                isTooChaotic = True
+            
+            else:
+                numberOfBribes += bribesTaken
+            
+    
+    if isTooChaotic:
         print("Too chaotic")
     else:
         print(numberOfBribes)
+    
+    return "----------------------"
 
 print(minimumBribes([2, 5, 1, 3, 4]))
+print(minimumBribes([5, 1, 2, 3, 7, 8, 6, 4]))
+print(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]))
