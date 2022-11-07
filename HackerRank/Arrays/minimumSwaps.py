1fd9679e8e72013714071bb48f7b338171b67e9c
Hannah Lormenyo <lormenyoh@gmail.com>
2022-11-07 21:46:04 +0000

    wrote the function for minimum swaps

--- HackerRank/Arrays/minimumSwaps.py
+++ HackerRank/Arrays/minimumSwaps.py
@@ -1,8 +1,16 @@
 
 
 def minimumSwaps(arr):
-    return
-
+    numberOfSwaps = 0
+    for i in range(len(arr)):
+        if i != arr[i]-1:
+            swapValue = arr[i]
+            swapIndex = arr.index(i+1)
+            arr[i] = i+1
+            arr[swapIndex] = swapValue
+            
+            numberOfSwaps += 1
+    return numberOfSwaps
 
 arr1 =  [7, 1, 3, 2, 4, 5, 6]
 arr2 = [4,3,1,2]
