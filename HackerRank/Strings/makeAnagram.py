7530379bddf7dba6da2fc9659ede17788341d6fa
Hannah Lormenyo <lormenyoh@gmail.com>
2022-12-05 22:47:39 +0000

    added file

--- HackerRank/Strings/makeAnagram.py
+++ HackerRank/Strings/makeAnagram.py
@@ -1,9 +1,15 @@
 
+#only works for 2 test cases
+def makeAnagram1(a, b):
+    diff = (len(a) - len(set(a))) + (len(b)-len(set(b)))
+    return len(set(a) ^ set(b)) + diff
 
-def makeAnagram(a, b):
-    return len(set(a) ^ set(b))
+def makeAnagram2(a,b):
+    a = sorted(list(a))
+    b = sorted(list(b))
 
+    return sum(aElement != bElement for aElement, bElement in zip(a, b) ) + abs(len(a)-len(b))
 
-print(makeAnagram("fcrxzwscanmligyxyvym", "jxwtrhvujlmrpdoqbisbwhmgpmeoke"))
+print(makeAnagram1("fcrxzwscanmligyxyvym", "jxwtrhvujlmrpdoqbisbwhmgpmeoke"))
 
     
