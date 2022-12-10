a1b61c2e33ac55b5c04d9d60883cc557bf8b3ff0
Hannah Lormenyo <lormenyoh@gmail.com>
2022-12-10 20:03:19 +0000

    added algo

--- leetcode/200. Number of Islands.py
+++ leetcode/200. Number of Islands.py
@@ -5,13 +5,46 @@
 # horizontally or vertically. You may assume all four edges of the grid are all 
 # surrounded by water.
 
+import collections
+
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
 
+        if not grid:
+            return 0
+
+        numberOfIslands = 0
+        visitedLands = set()
+
         def bfs(i, j):
-            return
+            queue = collections.deque()
+            visitedLands.add((i, j))
+            queue.append((i, j))
+
+            while queue:
+                r, c = queue.popleft()
+                directions = [
+                    [1, 0], #down
+                    [-1 , 0], #up
+                    [0, 1], #right
+                    [0, -1] #left
+                ]
+
+                for di, dj in directions:
+                    row, col = r+ di, c + dj
+                    if (row) in range(len(grid)) and \
+                    (col) in range(len(grid[0])) and \
+                    grid[row][col] == "1" and \
+                    (row, col) not in visitedLands:
+                       queue.append((row, col))
+                       visitedLands.add((row, col))
+            
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
                     bfs(i, j)
+
+                    numberOfIslands += 1
+
+        return numberOfIslands
