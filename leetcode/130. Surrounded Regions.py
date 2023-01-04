f3560c7b1d884ef602d8e7e2bd98f7f0caa204e6
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-04 05:04:19 +0000

    update

--- leetcode/130. Surrounded Regions.py
+++ leetcode/130. Surrounded Regions.py
@@ -49,3 +49,46 @@ class Solution:
             for c in range(cols):
                 if board[r][c] == "O" and (r, c) not in visited and r != 0 and c != 0 and c != cols-1 and r != rows-1 and checkForX(r,c):
                     bfs(r, c)
+
+
+
+class RealSolution:
+    def solve(self, board: list[list[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows = len(board)
+        cols = len(board[0])
+
+        # USING THE REVERSE THINKING APPROACH
+        # CAPTURE EVERYTHING EXCEPT THE UNSURROUNDED REGIONS
+
+        def dfs(r, c):
+            #if region is out of bounds, or the region is not O, then ignore
+            if r < 0 or c < 0 or r == rows or c == cols or board[r][c] != "O":
+                return
+
+            board[r][c] = "T"
+            dfs(r + 1, c) # down
+            dfs(r - 1, c) # up
+            dfs(r, c - 1) # left
+            dfs(r, c + 1) # right
+
+        # mark all unsurrounded regions at the border as T
+        for r in range(rows):
+            for c in range(cols):
+                if r == 0 or c == 0 or r == rows -1 or c == cols -1:
+                    dfs(r, c)
+
+        # flip all remaining Os in Xs
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+
+        #flip the Ts into Os
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
+
