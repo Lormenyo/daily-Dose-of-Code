5e8f05624a11c6d903ae9087e21c709db03dc87a
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-04 04:27:55 +0000

    update

--- leetcode/130. Surrounded Regions.py
+++ leetcode/130. Surrounded Regions.py
@@ -1,5 +1,10 @@
 import collections
 
+#this solution only passes 26/58 test cases
+# fails with this test case: board = [["X","O","X"],["X","O","X"],["X","O","X"]]
+# it returns [["X","O","X"],["X","X","X"],["X","O","X"]] instead of [["X","O","X"],["X","O","X"],["X","O","X"]]
+# if i modify the checkforX function to check if the 0s are surrounded by X, then it means
+# i will be adding 16 conditions (since the 0 has to be surroundered by ) at least 2 Xs
 class Solution:
     def solve(self, board: list[list[str]]) -> None:
         """
@@ -11,7 +16,6 @@ class Solution:
         visited = set()
 
         def checkForX(i, j):
-            ## check foerr all not one
             return board[i-1][j] == "X" or board[i+1][j] == "X" or board[i][j+1] == "X" or board[i][j-1] == "X"
 
         def bfs(r, c):
@@ -32,7 +36,7 @@ class Solution:
                     newRow, newCol = dr + row, dc + col
                       
 
-                    if newRow < (rows-1) and newCol < (cols-1) and board[newRow][newCol] == "O" and (newRow, newCol) not in visited and newRow not in [0, rows-1] and newCol not in [0, cols-1] and checkForX(newRow, newCol):
+                    if newRow < (rows-1) and newCol < (cols-1) and board[newRow][newCol] == "O" and (newRow, newCol) not in visited and newRow != 0 and newRow != 0 and newCol != cols-1 and newRow != rows-1:
                         board[newRow][newCol] = "X"
 
                         visited.add((newRow, newCol))
@@ -43,17 +47,5 @@ class Solution:
         #find the zero surrounded by x, flip to x
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "O" and (r, c) not in visited and r not in [0, rows-1] and c not in [0, cols-1]:
-                # if board[r][c] == "O" and (r, c) not in visited and r not in range(0, rows-1) and c not in range(0, cols-1):
+                if board[r][c] == "O" and (r, c) not in visited and r != 0 and c != 0 and c != cols-1 and r != rows-1 and checkForX(r,c):
                     bfs(r, c)
-
-
-
-# ["O","O"],
-# ["O","O"]
-
-# r != 0
-# c != 0
-# c != cols-1
-# r != rows - 1
-
