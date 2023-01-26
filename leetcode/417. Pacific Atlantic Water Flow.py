94bdda90f04b3f66f8c5513df94a16a87b143191
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-26 19:52:42 +0000

    solved pacific

--- leetcode/417. Pacific Atlantic Water Flow.py
+++ leetcode/417. Pacific Atlantic Water Flow.py
@@ -35,3 +35,35 @@
 # [4,0]: [4,0] -> Pacific Ocean 
 #        [4,0] -> Atlantic Ocean
 # Note that there are other possible paths for these cells to flow to the Pacific and Atlantic oceans.
+
+class Solution:
+    def pacificAtlantic(self, heights: list[list[int]]) -> list[list[int]]:
+
+        rows, cols = len(heights), len(heights[0])
+        pacific, atlantic = set(), set()
+
+        def dfs(r, c, visited, prevHeight):
+            if ((r,c) in visited or r < 0 or r == rows or c == cols or c < 0 or heights[r][c] < prevHeight):
+                return
+            
+            visited.add((r,c))
+            dfs(r + 1, c, visited, heights[r][c])
+            dfs(r - 1, c, visited, heights[r][c])
+            dfs(r, c + 1, visited, heights[r][c])
+            dfs(r, c - 1, visited, heights[r][c])
+
+        for c in range(cols):
+            dfs(0, c, pacific, heights[0][c])
+            dfs(rows - 1, c, atlantic, heights[rows-1][c])
+
+        for r in range(rows):
+            dfs(r, 0, pacific, heights[r][0])
+            dfs(r, cols-1, atlantic, heights[r][cols-1])
+
+        result = []
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pacific and (r, c) in atlantic:
+                    result.append([r,c])
+
+        return result
