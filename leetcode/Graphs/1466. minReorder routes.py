5cc2d9afb899ded25af411cefb4ef896b0b8873a
Hannah Lormenyo <lormenyoh@gmail.com>
2023-01-27 06:34:17 +0000

    solved

---
+++ leetcode/Graphs/1466. minReorder routes.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+
+        edges = {(a,b) for a,b in connections}
+
+        neighbors = {city:[] for city in range(n)}
+
+        edgeChanges = 0
+
+        visited = set()
+
+        for a,b in connections:
+            neighbors[a].append(b)
+            neighbors[b].append(a)
+
+        def dfs(city):
+            nonlocal edgeChanges
+
+            for neighbor in neighbors[city]:
+                if neighbor in visited:
+                    continue
+                if (neighbor, city) not in edges:
+                    edgeChanges += 1
+                
+                visited.add(neighbor)
+                dfs(neighbor)
+
+        visited.add(0)
+        dfs(0)
+        return edgeChanges
