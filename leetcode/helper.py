180958e9e24bb42b4972a8d7b21180bef124a8d5
Hannah Lormenyo <lormenyoh@gmail.com>
2023-07-06 23:19:41 +0100

    added helper

---
+++ leetcode/helper.py
@@ -0,0 +1,13 @@
+
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def constructListNode(listOfNodes):
+    for (i,node) in enumerate(listOfNodes):
+
+        if i == 1:
+            pass
